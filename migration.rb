7511423a9a3efb69c56451637f413397cb64ce65
Alaguraj-Saravanakumar <alaguraj.saravanakumar@freshworks.com>
2023-09-21 10:40:02 +0530

    review comments

--- migration.rb
+++ migration.rb
@@ -8,7 +8,6 @@ class MigrateWf
     @options = { payload_type: 'workflow_sync', meta: { 'source' => 'search' } }
   end
 
-
   def check_redis
     BaseRedis.key_exists?("STOP_WF_SEARCH_MIGRAION")
   end
@@ -31,29 +30,36 @@ class MigrateWf
     @req_counter += 1 if res
   end
 
-  def migrate_wf(shard_names, rpm)
-    shard_names.each do |shard_name|
-      Sharding.run_on_shard(shard_name) do
-        AccountIterator.each(batch_size: 200) do |account|
-          next unless account.active?
-          account.make_current
-          ActsAsTenant.current_tenant = account
-          Account.current.all_workflows.find_each(batch_size: 500) do |wf|
-            BaseRedis.set_key("WF_MIGRATION", Account.current.id) and break if check_redis
-            central_push(wf)
-            ensure_rpm if @req_counter >= rpm
-          end
-        rescue StandardError => e
-          puts "account_id: #{account.id}"
-          puts "exception: #{e.inspect}"
-          puts '----------------------------------'
-          @failed_account_ids.push(account.id)
-        ensure
-          break if check_redis
-          Account.reset_current
-        end
+  def migrate_wf(shard_name, rpm)
+    Sharding.run_on_shard(shard_name) do
+      if BaseRedis.key_exists?("WF_MIGRATION")
+        account = BaseRedis.get_key('WF_MIGRATION').to_i
+        Rails.logger.info "Fetching from redis key - account #{account}"
+        meta = { batch_size: 200, conditions: ['id >= ?', account] }
+      else
+        meta = { batch_size: 200 }
       end
-      puts "breaking since redis key presents" and break if check_redis
+      AccountIterator.each(meta) do |account|
+        next unless account.active?
+        Rails.logger.info "Running migration for account #{account.id}"
+        account.make_current
+        ActsAsTenant.current_tenant = account
+        BaseRedis.set_key("WF_MIGRATION", Account.current.id)
+        Account.current.all_workflows.find_each(batch_size: 300) do |wf|
+          break if check_redis
+          central_push(wf)
+          ensure_rpm if @req_counter >= rpm
+        end
+      rescue StandardError => e
+        puts "account_id: #{account.id}"
+        puts "exception: #{e.inspect}"
+        puts '----------------------------------'
+        @failed_account_ids.push(account.id)
+      ensure
+        puts "breaking since redis key presents" and break if check_redis
+        Account.reset_current
+      end
+      BaseRedis.remove_key("WF_MIGRATION")
     end
     puts "failed accounts - #{@failed_account_ids}"
   end
