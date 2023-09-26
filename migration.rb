5185fba93e2aee86baad7cf967b462359846e64e
Alaguraj-Saravanakumar <alaguraj.saravanakumar@freshworks.com>
2023-09-26 14:56:29 +0530

    loggers

--- migration.rb
+++ migration.rb
@@ -26,6 +26,7 @@ class MigrateWf
     payload[:event_uuid] = wf.event_uuid || wf.generate_event_id
     payload = { params: payload, version: 2 }
     res = FreshPipeReSync.new.perform(payload)
+    log("workflow: #{wf.id} res: #{res}", self.class.name)
     @req_counter += 1 if res
   end
 
@@ -33,27 +34,27 @@ class MigrateWf
     Sharding.run_on_shard(shard_name) do
       acc = BaseRedis.get_key('WF_MIGRATION')
       if acc.present?
-        Rails.logger.info "Fetching from redis key - account #{acc.to_i}"
         meta = { batch_size: 200, conditions: ['id >= ?', acc.to_i] }
       else
         meta = { batch_size: 200 }
       end
+      log(meta, 'META', self.class.name)
       AccountIterator.each(meta) do |account|
         next unless account.active?
-        Rails.logger.info "Running migration for account #{account.id}"
         account.make_current
         ActsAsTenant.current_tenant = account
         BaseRedis.set_key("WF_MIGRATION", Account.current.id)
+        log("MIGRATION_START", self.class.name)
         Account.current.all_workflows.find_each(batch_size: 300) do |wf|
           break if check_redis
           central_push(wf)
           ensure_rpm if @req_counter >= rpm
         end
+        log("MIGRATION_END", self.class.name)
       rescue StandardError => e
         puts "account_id: #{account.id}"
         puts "exception: #{e.inspect}"
         puts '----------------------------------'
-        BaseRedis.smembers('WF_MIGRATION_FAILED_ACCOUNT_IDS')
         BaseRedis.sadd('WF_MIGRATION_FAILED_ACCOUNT_IDS', account.id)
       ensure
         puts "breaking since redis key presents" and break if check_redis
@@ -61,7 +62,17 @@ class MigrateWf
       end
       BaseRedis.remove_key("WF_MIGRATION")
     end
-    puts "failed accounts - #{BaseRedis.get_key('WF_MIGRATION_FAILED_ACCOUNT_IDS')}"
+    puts "failed accounts - #{BaseRedis.smembers('WF_MIGRATION_FAILED_ACCOUNT_IDS')}"
     BaseRedis.remove_key('WF_MIGRATION_FAILED_ACCOUNT_IDS')
   end
+
+  def log(message, *additional_tags)
+    tags = [Account.current.try(:id)]
+    tags.concat(additional_tags)
+    Rails.logger.tagged(*tags) do
+      Rails.logger.info(message)
+    end
+  end
 end
+
+MigrateWf.new.migrate_wf('shard_1', 1000)
